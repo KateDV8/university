4230982b4f3a994742c5bd8a0a1ad07d8cdadcec
KateDV8 <pavlovae8@gmail.com>
2024-03-02 03:43:23 +0500

    creat: class professor

---
+++ src/main/java/ru/ekaterinadvoretskaia/university/entity/Professor.java
@@ -0,0 +1,10 @@
+package ru.ekaterinadvoretskaia.university.entity;
+
+import ru.ekaterinadvoretskaia.university.interfaces.Teach;
+
+public class Professor implements Teach {
+    @Override
+    public void teach() {
+
+    }
+}
