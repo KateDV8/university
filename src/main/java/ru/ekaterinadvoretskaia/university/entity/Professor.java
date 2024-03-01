84227a08c568c1b9775bac3437bb947e4e1fea6a
KateDV8 <pavlovae8@gmail.com>
2024-03-02 03:53:28 +0500

    fix: class student and professor

--- src/main/java/ru/ekaterinadvoretskaia/university/entity/Professor.java
+++ src/main/java/ru/ekaterinadvoretskaia/university/entity/Professor.java
@@ -1,10 +1,26 @@
 package ru.ekaterinadvoretskaia.university.entity;
 
 import ru.ekaterinadvoretskaia.university.interfaces.Teach;
+import ru.ekaterinadvoretskaia.university.interfaces.Visit;
 
-public class Professor implements Teach {
+public class Professor implements Teach, Visit {
     @Override
     public void teach() {
+
+    }
+
+    @Override
+    public void comeToUniversity() {
+
+    }
+
+    @Override
+    public void lunch() {
+
+    }
+
+    @Override
+    public void leaveUniversity() {
 
     }
 }
--- src/main/java/ru/ekaterinadvoretskaia/university/entity/Student.java
+++ src/main/java/ru/ekaterinadvoretskaia/university/entity/Student.java
@@ -1,11 +1,27 @@
 package ru.ekaterinadvoretskaia.university.entity;
 
 import ru.ekaterinadvoretskaia.university.interfaces.Study;
+import ru.ekaterinadvoretskaia.university.interfaces.Visit;
 
-public class Student implements Study {
+public class Student implements Study, Visit {
 
     @Override
     public void study() {
+
+    }
+
+    @Override
+    public void comeToUniversity() {
+
+    }
+
+    @Override
+    public void lunch() {
+
+    }
+
+    @Override
+    public void leaveUniversity() {
 
     }
 }
