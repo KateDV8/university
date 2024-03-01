b0b1f4425eb72cbe28140b5b52a6a0b3d0734a1a
KateDV8 <pavlovae8@gmail.com>
2024-03-02 03:46:11 +0500

    creat: class student

---
+++ src/main/java/ru/ekaterinadvoretskaia/university/entity/Student.java
@@ -0,0 +1,12 @@
+package ru.ekaterinadvoretskaia.university.entity;
+
+import ru.ekaterinadvoretskaia.university.interfaces.Study;
+
+public class Student implements Study {
+
+    @Override
+    public void study() {
+
+    }
+}
+
